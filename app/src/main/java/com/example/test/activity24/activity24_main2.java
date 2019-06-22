b63b55c40b18c02ad9e493ff0d607ba271c55c31
kulya91 <1942186814@qq.com>
2019-06-22 23:30:44 +0800

    通知的简单使用

---
+++ app/src/main/java/com/example/test/activity24/activity24_main2.java
@@ -0,0 +1,16 @@
+package com.example.test.activity24;
+
+import android.os.Bundle;
+
+import com.example.test.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity24_main2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity24_main2);
+    }
+}
